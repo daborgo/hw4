14412c0ae6179e0c0bddde6af02891f15bb34dfe
daborgo <diegoborgo14@gmail.com>
2024-03-24 14:12:23 -0700

    patient view and patient intake functionality implementation

--- src/hw4/hw4.java
+++ src/hw4/hw4.java
@@ -1,16 +1,18 @@
 package hw4;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -104,14 +106,39 @@ public class hw4 extends Application {
         
         
         Button save = new Button("Save");
+        Label errorLabel = new Label();
         save.setFont(Font.font("Verdana", 16));
         save.setMinWidth(100);
         save.setMinHeight(30);
         save.setOnAction(e -> {
-            System.out.println("- Saved data!");
+        	if(fnField.getText().isEmpty() || lnField.getText().isEmpty() || emailField.getText().isEmpty() || pnField.getText().isEmpty() || hhField.getText().isEmpty() || idField.getText().isEmpty()) {
+        		errorLabel.setText("Error: One or more fields are empty.");
+        	} else {
+        		Random r = new Random();
+            	String patientID = String.valueOf(r.nextInt(90000)+10000);
+            	FileWriter fw;
+				try {
+					fw = new FileWriter(patientID + "_PatientInfo.txt");
+	            	fw.write(fnField.getText() + "\n");
+	            	fw.write(lnField.getText() + "\n");
+	            	fw.write(emailField.getText() + "\n");
+	            	fw.write(pnField.getText() + "\n");
+	            	fw.write(hhField.getText() + "\n");
+	            	fw.write(idField.getText() + "\n");
+	            	fw.close();
+	            	save.setDisable(true);
+	            	save.setVisible(false);
+				} catch (IOException e1) {
+					System.out.println("Error: Write failed.");
+					e1.printStackTrace();
+				}
+            	
+            	errorLabel.setText("Saved data! Patient ID: " + patientID);
+                System.out.println("- Saved data!");
+        	}
         });
 
-        intakeUI.getChildren().addAll(columns, save);
+        intakeUI.getChildren().addAll(columns, save, errorLabel);
 
         Scene scene = new Scene(intakeUI, 400, 400);
         primStage.setScene(scene);
@@ -121,8 +148,8 @@ public class hw4 extends Application {
         System.out.println("Opening CT Scan...");
         primStage.setTitle("Technician View");
         
-        VBox intakeUI = new VBox(10);
-        intakeUI.setSpacing(5);
+        VBox scanUI = new VBox(10);
+        scanUI.setSpacing(5);
 
         VBox labels1 = new VBox(20);
         Label idLabel = new Label("Patient ID:");
@@ -140,8 +167,8 @@ public class hw4 extends Application {
         columns1.setPadding(new Insets(40,0,10,20));
         columns1.getChildren().addAll(labels1, fields1);
         
-        Text title = new Text("    Vessel Level Agatston CAC score");
-        title.setFont(Font.font("Verdana", 14));
+        Text header = new Text("    Vessel Level Agatston CAC score");
+        header.setFont(Font.font("Verdana", 14));
         
         VBox labels2 = new VBox(20);
         Label lmLabel = new Label("LM:");
@@ -174,13 +201,58 @@ public class hw4 extends Application {
         });
         saveBox.getChildren().add(save);
 
-        intakeUI.getChildren().addAll(columns1, title, columns2, saveBox);
+        scanUI.getChildren().addAll(columns1, header, columns2, saveBox);
 
-        Scene scene = new Scene(intakeUI, 470, 400);
+        Scene scene = new Scene(scanUI, 470, 400);
         primStage.setScene(scene);
     }
 
     private void patientView() {
         System.out.println("Opening Patient View...");
+        primStage.setTitle("Patient View");
+        
+        VBox patientUI = new VBox(10);
+        patientUI.setSpacing(5);
+        
+        HBox titleBox = new HBox(10);
+        titleBox.setAlignment(Pos.CENTER);
+        Text title = new Text("Hello <Patient Name>");
+        title.setFont(Font.font("Verdana", 14));
+        titleBox.getChildren().add(title);
+        
+        HBox scoreBox = new HBox(20);
+        Label scoreLabel = new Label("The total Agatston CAC score");
+        TextArea scoreField = new TextArea();
+        scoreField.setEditable(false);
+        scoreField.setPrefWidth(240);
+        scoreField.setPrefHeight(5);
+        scoreBox.setPadding(new Insets(20,0,10,20));
+        scoreBox.getChildren().addAll(scoreLabel, scoreField);
+        
+        VBox labels = new VBox(20);
+        Label lmLabel = new Label("LM:");
+        Label ladLabel = new Label("LAD:");
+        Label lcxLabel = new Label("LCX:");
+        Label rcaLabel = new Label("RCA:");
+        Label pdaLabel = new Label("PDA:");
+        labels.getChildren().addAll(lmLabel, ladLabel, lcxLabel, rcaLabel, pdaLabel);
+        
+        VBox fields = new VBox(10);
+        TextField lmField = new TextField();
+        TextField ladField = new TextField();
+        TextField lcxField = new TextField();
+        TextField rcaField = new TextField();
+        TextField pdaField = new TextField();
+        fields.getChildren().addAll(lmField, ladField, lcxField, rcaField, pdaField);
+        
+        HBox columns = new HBox(20);
+        columns.setPadding(new Insets(10,0,10,20));
+        columns.getChildren().addAll(labels, fields);
+        
+        patientUI.getChildren().addAll(titleBox, scoreBox, columns);
+        patientUI.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(patientUI, 470, 400);
+        primStage.setScene(scene);
     }
 }
